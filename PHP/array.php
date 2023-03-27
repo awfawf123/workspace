9c287f55e0d2d9f037752cbcc5525cda0714880f
awfawf123 <awfawf123@naver.com>
2023-03-27 19:16:53 +0900

    array

--- PHP/array.php
+++ PHP/array.php
@@ -45,4 +45,51 @@
     $arr_week = array("Sun","Mon","delete","Tue","Wed");
     unset($arr_week[2]);
     // print_r($arr_week);
+
+    //중복되지 않는 원소를 반환
+    $arr_diff_1 = array("a","b","c");
+    $arr_diff_2 = array("a","b","d");
+    $arr_diff = array_diff($arr_diff_1, $arr_diff_2);
+    // print_r($arr_diff);
+    
+    //배열의 정렬 asort(), arsort(), ksort(), krsort()
+    $arr_asort = array("b","a","d","c");
+    asort($arr_asort);
+    // print_r($arr_asort);
+
+    $arr_arsort = array("b","a","d","c");
+    arsort($arr_arsort);
+    // print_r($arr_arsort);
+
+    $arr_ksort = array("key1" => "val1",
+                     "key3" => "val3",
+                     "key2" => "val2"
+                    );
+    ksort($arr_ksort);
+    // print_r($arr_ksort);
+
+    $arr_krsort = array("key1" => "val1",
+                     "key3" => "val3",
+                     "key2" => "val2",
+                     "key4" => "val4"
+                    );
+    krsort($arr_krsort);
+    // print_r($arr_krsort);
+
+    // array의 사이즈를 반환하는 함수
+    // echo count($arr_krsort);
+
+    //foreach
+    $arr1 = array("a" => "1",
+                     "b" => "2",
+                     "c" => "3",
+                     "d" => "4"
+                 );
+
+    // foreach($arr1 as $key => $val){
+    //     echo $key.$val."\n";
+    // }
+    foreach( $arr1 as $val){
+        echo $val;
+    }
 ?>
--- PHP/test/array.php
+++ PHP/test/array.php
@@ -18,5 +18,26 @@
                             ,"김치" => "김치"
                             ,"라면" => "면");
     unset($arr_asso_del["김치"]);
-    print_r($arr_asso_del);
+    // print_r($arr_asso_del);
+
+    $arr_asso_for = array("볶음밥" =>"밥"
+                            ,"된장찌개" => "된장"
+                            ,"삭제" => "값값"
+                            ,"김치" => "김치"
+                            ,"라면" => "면"
+                        );
+    // foreach문을 이용해서 키가 "삭제"인 요소를 제거
+    // if문 사용, unset("삭제") X, 키가 "삭제"이외는 "키 : 값" 포맷으로 출력
+    foreach ($arr_asso_for as $key => $val) {
+        if($key === "삭제"){
+            // echo $key;
+            unset($arr_asso_for[$key]);
+            // unset($arr_asso_for["삭제"]);
+        }else{
+            echo $key." : ".$val."\n";
+        }
+        // var_dump($arr_asso_for);
+    }
+    
+    
 ?>
