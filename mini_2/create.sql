6177ad0179582b676c10a0e17120b73b2e81824d
awfawf123 <awfawf123@naver.com>
2023-05-18 18:14:25 +0900

    05/18

--- mini_2/create.sql
+++ mini_2/create.sql
@@ -12,6 +12,17 @@ CREATE TABLE user_info(
 	,u_num CHAR(12) NOT NULL
 );
 
+CREATE TABLE list_info(
+	li_no INT PRIMARY KEY AUTO_INCREMENT
+	,li_name VARCHAR(100) NOT NULL
+	,li_end_date DATE 
+	,li_cont VARCHAR(512)
+	,li_price INT
+	,li_category VARCHAR(20) NOT NULL
+	,li_imgfile VARCHAR(100)
+);
+
+DROP TABLE user_info;
 
 DELETE FROM user_info WHERE u_id="user";
 
@@ -30,6 +41,22 @@ INSERT INTO user_info(
 	,'awfawf123@naver.com'
 	,01012345678
 );
+
+INSERT INTO list_info(
+	li_name
+	,li_end_date
+	,li_cont
+	,li_price
+	,li_category
+)VALUES(
+	'커피'
+	,DATE(20230518)
+	,'향긋한 원두를 사용한 커피'
+	,30000
+	,'신제품'
+);
+
+ALTER TABLE user_info ADD COLUMN u_auth VARCHAR(10) NOT NULL;
 
 SELECT * FROM user_info;
 
