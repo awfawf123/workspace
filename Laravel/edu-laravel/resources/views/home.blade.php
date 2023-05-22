7c5db7dab27118cdd5576072e780a2e9cbb7e2db
awfawf123 <awfawf123@naver.com>
2023-05-22 17:42:15 +0900

    05/22 Laravel

---
+++ Laravel/edu-laravel/resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>home</h1>
+    <br>
+    <br>
+    <br>
+    <form action="/method" method="post">
+        @csrf 
+        <button type="submit">POST</button>
+    </form>
+    <br>
+    <form action="/method" method="post">
+        @csrf
+        @method('PUT')
+        <button type="submit">PUT</button>
+    </form>
+    <br>
+    <form action="/method" method="post">
+        @csrf
+        @method('DELETE')
+        <button type="submit">DELETE</button>
+    </form>
+</body>
+</html>
